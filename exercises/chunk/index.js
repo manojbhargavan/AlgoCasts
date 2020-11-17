200617cd309d770eca892e21d58d3a2e32492b93
Manoj Bhargavan <manojbhargavan07@gmail.com>
2020-11-17 21:13:01 +0530

    few problems solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   let arrayLength = array.length;
+//   let chunkArray = [];
+
+//   for (let i = 0; i < arrayLength; i++) {
+//     let subArray = [];
+//     for (let j = 0; j < size; j++) {
+//       subArray.push(array[i]);
+//       if (j + 1 != size) i++;
+//       if (i >= arrayLength) break;
+//     }
+//     chunkArray.push(subArray);
+//   }
+//   return chunkArray;
+// }
+
+// function chunk(array, size) {
+//   let chunked = [];
+//   for (let element of array) {
+//     let lastChunk = chunked[chunked.length - 1];
+
+//     if (!lastChunk || lastChunk.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       lastChunk.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,36 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 15 === 0) {
+//       console.log("fizzbuzz");
+//     } else {
+//       if (i % 3 === 0) {
+//         console.log("fizz");
+//       } else {
+//         if (i % 5 === 0) {
+//           console.log("buzz");
+//         } else {
+//           console.log(i);
+//         }
+//       }
+//     }
+//   }
+// }
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(
+      i % 15 === 0
+        ? "fizzbuzz"
+        : i % 3 === 0
+        ? "fizz"
+        : i % 5 === 0
+        ? "buzz"
+        : i
+    );
+  }
+}
 
 module.exports = fizzBuzz;
